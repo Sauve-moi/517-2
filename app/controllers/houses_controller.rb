0a92e9550f6e0e6ea334becca4c61d7c6f5197ab
Sauve-moi <yzzczy@gmail.com>
2018-10-01 00:48:34 -0400

    houses, pictures

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,97 @@
+class HousesController < ApplicationController
+  before_action :set_house, only: [:show, :edit, :update, :destroy]
+
+  # GET /houses
+  # GET /houses.json
+
+
+  def index
+    @houses = House.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @houses }
+    end
+  end
+
+  # GET /houses/1
+  # GET /houses/1.json
+  def show
+
+    @picture = Picture.find_by(house_id: @house.house_id)
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @house }
+    end
+  end
+
+  # GET /houses/new
+  def new
+    @house = House.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @house }
+    end
+  end
+
+  # GET /houses/1/edit
+  def edit
+    #@house = House.find(params[:house_id])
+  end
+
+  # POST /houses
+  # POST /houses.json
+  def create
+    @house = House.new(house_params)
+
+    respond_to do |format|
+      if @house.save
+        format.html { redirect_to @house, notice: 'House was successfully created.' }
+        format.json { render :show, status: :created, location: @house }
+      else
+        format.html { render action :"new" }
+        format.json { render json: @house.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /houses/1
+  # PATCH/PUT /houses/1.json
+  def updatemain
+
+    respond_to do |format|
+      if @house.update(house_params)
+        format.html { redirect_to @house, notice: 'House was successfully updated.' }
+        format.json { render :show, status: :ok, location: @house }
+      else
+        format.html { render :edit }
+        format.json { render json: @house.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /houses/1
+  # DELETE /houses/1.json
+  def destroy
+    #@house = House.find(params[:house_id])
+    @house.destroy
+
+    respond_to do |format|
+      format.html { redirect_to houses_url, notice: 'House was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_house
+   # @house = House.find(params[:id]
+    @house=House.find_by(house_id: params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def house_params
+    params.require(:house).permit(:house_id, :location, :square_footage, :year_built, :style, :list_price, :floors, :basement, :current_house_owner, :contact_information_for_listing_realtor)
+  end
+end
---
+++ app/views/houses/_house.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! house, :house_id, :created_at, :updated_at
+json.url house_url(house, format: :json)
